05fb12d3f69235fefed4e4ecd67e07df4a17dc12
ABHI <siridoro1090@gmail.com>
2021-08-30 15:02:25 +1000

    Second Version of the program

---
+++ FirstRepo/ChandanaCalculations.py
@@ -0,0 +1,4 @@
+print("Hi this is Siri")
+print("I am trying to learn push and pull operations in github")
+print(" I understood how init, add and commit operations are done")
+print("Now I am trying to observe how push operation is implemented")
